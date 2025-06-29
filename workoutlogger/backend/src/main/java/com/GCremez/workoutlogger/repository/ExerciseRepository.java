4d7b6cfedf24206fe04a7f6c405c7defa44908cf
GCremez <salauolorunjuedalo@gmail.com>
2025-06-29 14:36:38 +0300

    Updating Features

--- workoutlogger/backend/src/main/java/com/GCremez/workoutlogger/repository/ExerciseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.GCremez.workoutlogger.repository;
-
-import com.GCremez.workoutlogger.model.Exercise;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
-}
